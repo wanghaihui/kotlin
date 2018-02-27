52fff8b56fa73d2e45e1d7919f22664cf4aaf745
wanghaihui <465495722@qq.com>
2018-02-27 15:51:35 +0800

    status bar translucant

--- app/src/main/java/com/imagine/story/common/base/BaseActivity.java
+++ app/src/main/java/com/imagine/story/common/base/BaseActivity.java
@@ -1,7 +1,9 @@
 package com.imagine.story.common.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 /**
  * Created by conquer on 2018/1/19.
@@ -12,6 +14,12 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+
         initViews();
     }
 
